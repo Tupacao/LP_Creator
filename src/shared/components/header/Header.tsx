4d4b1edcdd063edb544d8bd6bb39226b6327414b
dtiDigital <guilherme.yago@dtidigital.com.br>
2025-03-21 16:50:29 -0300

    mais algumas configurações

--- src/shared/components/header/Header.tsx
+++ src/shared/components/header/Header.tsx
@@ -1,54 +1,57 @@
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import logo from '../../assets/lp_logo.png'; // Certifique-se de que o caminho para a logo está correto
 
 const Header: React.FC = () => {
   return (
     <AppBar
-      position="static"
+      position='static'
       sx={{
-        backgroundColor: "background.default",
+        backgroundColor: 'background.default',
         boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
       }}
     >
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
-        <Typography variant="h6" sx={{ fontWeight: 'bold', color: "primary.main" }}>
-          LP Creator
-        </Typography>
+      <Toolbar
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          flexWrap: 'wrap',
+        }}
+      >
+        <Box component={Link} to='/home'>
+          <img src={logo} alt='LP Creator Logo' style={{ height: '65px', width: '65px' }} />
+        </Box>
         <Box sx={{ display: 'flex', gap: 2 }}>
           <Button
             component={Link}
-            to="/home"
-            sx={{ color: "primary.main", textDecoration: 'none' }}
+            to='/home'
+            sx={{ color: 'primary.main', textDecoration: 'none' }}
           >
             Home
           </Button>
           <Button
             component={Link}
-            to="/creator"
-            sx={{ color: "primary.main", textDecoration: 'none' }}
+            to='/creator'
+            sx={{ color: 'primary.main', textDecoration: 'none' }}
           >
             Creator
           </Button>
           <Button
             component={Link}
-            to="/sites"
-            sx={{ color: "primary.main", textDecoration: 'none' }}
+            to='/sites'
+            sx={{ color: 'primary.main', textDecoration: 'none' }}
           >
             Sites
           </Button>
         </Box>
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-        <Button
-            component={Link}
-            to="/user"
-            sx={{ textDecoration: 'none' }}
-          >
+          <Button component={Link} to='/user' sx={{ textDecoration: 'none' }}>
             <Box
               sx={{
-                backgroundColor: "primary.main",
-                color: "background.default",
+                backgroundColor: 'primary.main',
+                color: 'background.default',
                 borderRadius: '50%',
                 width: 40,
                 height: 40,
@@ -57,7 +60,7 @@ const Header: React.FC = () => {
                 justifyContent: 'center',
               }}
             >
-              <FontAwesomeIcon icon={faUser}  />
+              <FontAwesomeIcon icon={faUser} />
             </Box>
           </Button>
         </Box>
