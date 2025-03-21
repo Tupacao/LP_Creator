ea0305d985214d7167fa89f4aab996821f36ab85
dtiDigital <guilherme.yago@dtidigital.com.br>
2025-03-21 17:17:43 -0300

    refatorando footer

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -23,11 +23,7 @@ const Home: React.FC = () => {
           Bem-vindo ao LP_Creator
         </Typography>
       </Box>
-      <Footer
-        title='Footer'
-        text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-        buttonText='Botão'
-      />
+      <Footer />
     </Box>
   );
 };
