e91397ac8e842323efe4d1df895bd6d98f7b1ee3
Tupacao <arthur12268@gmail.com>
2025-03-24 13:48:52 -0300

    Fim login

---
+++ src/shared/styles/GlobalStyle.tsx
@@ -0,0 +1,15 @@
+import { css } from "@emotion/react";
+
+export default css`
+    *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+    html, body, #root {
+        max-height: 100vh;
+        max-width: 100vw;
+        width: 100%;
+        height: 100%;
+    }
+`
